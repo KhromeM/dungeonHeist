df89ebde893e081e83839414128821be921744fc
Siv <var.mironov@gmail.com>
2024-12-15 23:44:36 -0300

    top login bar

--- dungeon-heist/src/components/DungeonHeader.jsx
+++ dungeon-heist/src/components/DungeonHeader.jsx
@@ -3,9 +3,11 @@ import '../index.css';
 
 const LoginHeader = () => {
   return (
-    <div className="flex justify-between items-center bg-green-500 py-3 px-6">
-      <h1 className="text-lg font-Digitalt text-white">DUNGEON HEIST</h1>
-      <button className="font-Digitalt text-white">LOGIN</button>
+    <div className="absolute top-4 left-0 right-0 flex justify-between items-center bg-green-500 py-3 px-12 w-[50%] mx-auto z-10 rounded-lg">
+      <h1 className="text-4xl font-Digitalt text-white">DUNGEON HEIST</h1>
+      <button className="text-4xl font-Digitalt text-white bg-transparent hover:bg-transparent hover:outline-none p-0">
+        LOGIN
+      </button>
     </div>
   );
 };
